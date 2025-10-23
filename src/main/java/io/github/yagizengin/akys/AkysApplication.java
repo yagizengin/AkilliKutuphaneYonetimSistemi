2e0ed35f716b8782606872f5ec059dc1a74b585d
yagizengin <yagizengin06@outlook.com>
2025-10-24 02:27:32 +0300

    first commit

---
+++ src/main/java/io/github/yagizengin/akys/AkysApplication.java
@@ -0,0 +1,13 @@
+package io.github.yagizengin.akys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/yagizengin/akys/AkysApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yagizengin.akys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
